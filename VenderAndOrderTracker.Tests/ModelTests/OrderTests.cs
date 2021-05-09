30f29fd4ecc9a3f162ce19d9729d33690316481a
Shanen Cross <shanencross@yahoo.com>
2021-05-09 01:51:03 -0700

    Write and pass constructor tests for Vendor and Order

--- VenderAndOrderTracker.Tests/ModelTests/OrderTests.cs
+++ VenderAndOrderTracker.Tests/ModelTests/OrderTests.cs
@@ -6,6 +6,13 @@ namespace VendorAndOrderTracker.ModelTests
   [TestClass]
   public class OrderTests
   {
-    
+    [TestMethod]
+    public void VendorConstructor_CreatesInstanceOfVendor_Vendor() {
+      // Arrange and Act
+      Order newOrder = new Order();
+
+      // Assert
+      Assert.AreEqual(typeof(Order), newOrder.GetType());
+    }
   }
 }
--- VenderAndOrderTracker.Tests/ModelTests/VendorTests.cs
+++ VenderAndOrderTracker.Tests/ModelTests/VendorTests.cs
@@ -6,6 +6,15 @@ namespace VendorAndOrderTracker.ModelTests
   [TestClass]
   public class VendorTests
   {
-    
+    [TestMethod]
+    public void VendorConstructor_CreatesInstanceOfVendor_Vendor() {
+      // Arrange and Act
+      Vendor newVendor = new Vendor();
+
+      // Assert
+      Assert.AreEqual(typeof(Vendor), newVendor.GetType());
+    }
+
+
   }
 }
