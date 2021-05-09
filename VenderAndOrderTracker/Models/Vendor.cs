b9584d2871d4d907ac2fb7c9b1b820fbe9d05810
Shanen Cross <shanencross@yahoo.com>
2021-05-09 03:26:48 -0700

    Add Vendor class contents

--- VenderAndOrderTracker/Models/Vendor.cs
+++ VenderAndOrderTracker/Models/Vendor.cs
@@ -1,7 +1,22 @@
+using System.Collections.Generic;
+
 namespace VendorAndOrderTracker.Models
 {
   public class Vendor
   {
+    public string Name { get; }
+    public string Description { get; }
+    public List<Order> Orders { get; } = new List<Order>();
 
+    public Vendor(string name, string description)
+    {
+      Name = name;
+      Description = description;
+    }
+
+    public void AddOrder(Order order)
+    {
+      Orders.Add(order);
+    }
   }
 }
