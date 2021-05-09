b2b11ae60ab485da815dee60c36fdb6ad2860be1
Shanen Cross <shanencross@yahoo.com>
2021-05-09 04:06:37 -0700

    Add page listing vendors

--- VenderAndOrderTracker/Controllers/VendorsController.cs
+++ VenderAndOrderTracker/Controllers/VendorsController.cs
@@ -1,10 +1,16 @@
 using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
 using VendorAndOrderTracker.Models;
 
 namespace VendorAndOrderTracker.Controllers
 {
   public class VendorsController : Controller
   {
-    
+    [HttpGet("/vendors")]
+    public ActionResult Index()
+    {
+      List<Vendor> allVendors = Vendor.GetAll();
+      return View(allVendors);
+    }
   }
 }
