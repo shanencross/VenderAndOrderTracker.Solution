e6aaaf9661859f8a32b602abdb9a3c2238f64c94
Shanen Cross <shanencross@yahoo.com>
2021-05-09 05:55:29 -0700

    Add route for adding new order and returning to vendor view

--- VenderAndOrderTracker/Controllers/OrdersController.cs
+++ VenderAndOrderTracker/Controllers/OrdersController.cs
@@ -1,4 +1,5 @@
 using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
 using VendorAndOrderTracker.Models;
 
 namespace VendorAndOrderTracker.Controllers
@@ -10,6 +11,15 @@ namespace VendorAndOrderTracker.Controllers
     {
       Vendor vendor = Vendor.Find(id);
       return View(vendor);
+    }
+
+    [HttpPost("/vendors/{id}/orders")]
+    public ActionResult Create(int id, string title, string description, float price, string date)
+    {
+      Vendor vendor = Vendor.Find(id);
+      Order order = new Order(title, description, price, date);
+      vendor.AddOrder(order);
+      return RedirectToAction("Show", "Vendors", new { id = id });
     }
   }
 }
