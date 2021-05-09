2136977fbf4b8e78ef98bd549526389512673a9e
Shanen Cross <shanencross@yahoo.com>
2021-05-09 02:52:10 -0700

    Corrected tests so that they uses instances of Order

--- VenderAndOrderTracker.Tests/ModelTests/OrderTests.cs
+++ VenderAndOrderTracker.Tests/ModelTests/OrderTests.cs
@@ -13,37 +13,37 @@ namespace VendorAndOrderTracker.ModelTests
       testOrder = new Order("Croissants", "30 boxes of butter croissants", 100.99f, "February 7, 2025");
     }
     [TestMethod]
-    public void VendorConstructor_CreatesInstanceOfVendor_Vendor() {
-      Order newOrder = new Order();
-      Assert.AreEqual(typeof(Order), newOrder.GetType());
+    public void VendorConstructor_CreatesInstanceOfVendor_Vendor() 
+    {
+      Assert.AreEqual(typeof(Order), testOrder.GetType());
     }
 
     [TestMethod]
     public void GetTitle_ReturnsTitle_String()
     {
       string expectedTitle = "Croisssants";
-      Assert.AreEqual(Order.Title, expectedTitle);
+      Assert.AreEqual(testOrder.Title, expectedTitle);
     }
 
     [TestMethod]
     public void GetDescription_ReturnsDescription_String()
     {
       string expectedDescription = "30 boxes of butter croissants";
-      Assert.AreEqual(Order.Description, expectedDescription);
+      Assert.AreEqual(testOrder.Description, expectedDescription);
     }
 
     [TestMethod]
     public void GetPrice_ReturnsPrice_Int()
     {
       float expectedPrice = 100.99f;
-      Assert.AreEqual(Order.Price, expectedPrice);
+      Assert.AreEqual(testOrder.Price, expectedPrice);
     }
 
     [TestMethod]
     public void GetDate_ReturnsDate_String()
     {
-      float expectedDate = "Feburary 7, 2025";
-      Assert.AreEqual(Order.Date, expectedDate);
+      string expectedDate = "Feburary 7, 2025";
+      Assert.AreEqual(testOrder.Date, expectedDate);
     }
   }
 }
