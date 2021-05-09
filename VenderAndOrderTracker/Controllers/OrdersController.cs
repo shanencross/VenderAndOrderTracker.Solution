f2fed8eda3ff845241a9354720b978a35673be8a
Shanen Cross <shanencross@yahoo.com>
2021-05-09 03:32:53 -0700

    Add empty controllers

---
+++ VenderAndOrderTracker/Controllers/OrdersController.cs
@@ -0,0 +1,10 @@
+using Microsoft.AspNetCore.Mvc;
+using VendorAndOrderTracker.Models;
+
+namespace VendorAndOrderTracker.Controllers
+{
+  public class OrdersController : Controller
+  {
+    
+  }
+}
---
+++ VenderAndOrderTracker/Controllers/VendorsController.cs
@@ -0,0 +1,10 @@
+using Microsoft.AspNetCore.Mvc;
+using VendorAndOrderTracker.Models;
+
+namespace VendorAndOrderTracker.Controllers
+{
+  public class VendorsController : Controller
+  {
+    
+  }
+}
