4156e5953bd3dd1fd30b3b2ea9e8f0d40f6185e8
Shanen Cross <shanencross@yahoo.com>
2021-05-09 03:55:21 -0700

    Add tests for GetAll and ClearAll in Vendor

--- VenderAndOrderTracker.Tests/ModelTests/VendorTests.cs
+++ VenderAndOrderTracker.Tests/ModelTests/VendorTests.cs
@@ -1,12 +1,17 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
 using System.Collections.Generic;
 using VendorAndOrderTracker.Models;
 
 namespace VendorAndOrderTracker.ModelTests
 {
   [TestClass]
-  public class VendorTests
+  public class VendorTests : IDisposable
   {
+    public void Dispose()
+    {
+      Vendor.ClearAll();
+    }
     private Vendor testVendor;
     public VendorTests()
     {
@@ -55,7 +60,31 @@ namespace VendorAndOrderTracker.ModelTests
         Assert.AreEqual(order.Date, expectedOrder.Date);
       }
     }
+    
+    [TestMethod]
+    public void GetAll_ReturnsAllInstancesOfVendor_VendorList()
+    {
+      Vendor otherTestVendor = new Vendor("Bob's Bread Stand", "A convenient little outdoor bread stand!");
+      Order testOrder1 = new Order("Sourdough", "12 bags of sourdough bread", 50.99f, "February 3, 2025");
+      Order testOrder2 = new Order("Ciabatta", "20 bags of ciabatta bread", 28.99f, "March 3, 2025");
+      otherTestVendor.AddOrder(testOrder1);
+      otherTestVendor.AddOrder(testOrder2);
 
+      List<Vendor> expectedVendorList = new List<Vendor> { testVendor, otherTestVendor };
+      CollectionAssert.AreEqual(Vendor.GetAll(), expectedVendorList);
+    }
 
+    [TestMethod]
+    public void ClearAll_EmptiesListOfVendorInstances_VendorList()
+    {
+      Vendor otherTestVendor = new Vendor("Bob's Bread Stand", "A convenient little outdoor bread stand!");
+      Order testOrder1 = new Order("Sourdough", "12 bags of sourdough bread", 50.99f, "February 3, 2025");
+      Order testOrder2 = new Order("Ciabatta", "20 bags of ciabatta bread", 28.99f, "March 3, 2025");
+      otherTestVendor.AddOrder(testOrder1);
+      otherTestVendor.AddOrder(testOrder2);
+
+      List<Vendor> expectedVendorList = new List<Vendor> { testVendor, otherTestVendor };
+      CollectionAssert.AreEqual(Vendor.GetAll(), expectedVendorList);
+    }
   }
 }
