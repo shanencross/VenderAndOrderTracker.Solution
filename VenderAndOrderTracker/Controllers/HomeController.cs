c1b8f8ddfc5d1105d1e8f9cb9e6989818457aa95
Shanen Cross <shanencross@yahoo.com>
2021-05-09 03:37:13 -0700

    Add splash page

--- VenderAndOrderTracker/Controllers/HomeController.cs
+++ VenderAndOrderTracker/Controllers/HomeController.cs
@@ -5,6 +5,10 @@ namespace VendorAndOrderTracker.Controllers
 {
   public class HomeController : Controller
   {
-    
+    [HttpGet("/")]
+    public ActionResult Index()
+    {
+      return View();
+    }
   }
 }
