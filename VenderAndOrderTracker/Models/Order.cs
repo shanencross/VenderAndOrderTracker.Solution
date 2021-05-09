bc01893f8cacba61611a6b6c523a72ac792cd30c
Shanen Cross <shanencross@yahoo.com>
2021-05-09 02:52:25 -0700

    Add Order properties and constructor

--- VenderAndOrderTracker/Models/Order.cs
+++ VenderAndOrderTracker/Models/Order.cs
@@ -2,7 +2,18 @@ namespace VendorAndOrderTracker.Models
 {
   public class Order
   {
-
+    public string Title { get; set; }
+  
+    public string Description { get; set; }
+    public float Price { get; set; }
+    public string Date { get; set; }
+    public Order(string title, string description, float price, string date)
+    {
+      Title = title;
+      Description = description;
+      Price = price;
+      Date = date;
+    }
   }
 
 }
