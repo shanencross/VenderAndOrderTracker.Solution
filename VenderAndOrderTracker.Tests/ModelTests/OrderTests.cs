1b02eec832c314d37a3583c023b9a23ada69f188
Shanen Cross <shanencross@yahoo.com>
2021-05-09 02:44:20 -0700

    Add tests for Order and Vendor

--- VenderAndOrderTracker.Tests/ModelTests/OrderTests.cs
+++ VenderAndOrderTracker.Tests/ModelTests/OrderTests.cs
@@ -6,13 +6,44 @@ namespace VendorAndOrderTracker.ModelTests
   [TestClass]
   public class OrderTests
   {
+    private Order testOrder;
+    
+    public OrderTests()
+    {
+      testOrder = new Order("Croissants", "30 boxes of butter croissants", 100.99f, "February 7, 2025");
+    }
     [TestMethod]
     public void VendorConstructor_CreatesInstanceOfVendor_Vendor() {
-      // Arrange and Act
       Order newOrder = new Order();
-
-      // Assert
       Assert.AreEqual(typeof(Order), newOrder.GetType());
+    }
+
+    [TestMethod]
+    public void GetTitle_ReturnsTitle_String()
+    {
+      string expectedTitle = "Croisssants";
+      Assert.AreEqual(Order.Title, expectedTitle);
+    }
+
+    [TestMethod]
+    public void GetDescription_ReturnsDescription_String()
+    {
+      string expectedDescription = "30 boxes of butter croissants";
+      Assert.AreEqual(Order.Description, expectedDescription);
+    }
+
+    [TestMethod]
+    public void GetPrice_ReturnsPrice_Int()
+    {
+      float expectedPrice = 100.99f;
+      Assert.AreEqual(Order.Price, expectedPrice);
+    }
+
+    [TestMethod]
+    public void GetDate_ReturnsDate_String()
+    {
+      float expectedDate = "Feburary 7, 2025";
+      Assert.AreEqual(Order.Date, expectedDate);
     }
   }
 }
--- VenderAndOrderTracker.Tests/ModelTests/VendorTests.cs
+++ VenderAndOrderTracker.Tests/ModelTests/VendorTests.cs
@@ -1,4 +1,5 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
 using VendorAndOrderTracker.Models;
 
 namespace VendorAndOrderTracker.ModelTests
@@ -6,13 +7,44 @@ namespace VendorAndOrderTracker.ModelTests
   [TestClass]
   public class VendorTests
   {
+    private Vendor testVendor;
+    public VendorTests()
+    {
+      testVendor = new Vendor("Suzie's Cafe", "A happy and cozy little cafe!");
+      testOrder1 = new Order("Croissants", "30 boxes of butter croissants", 100.99f, "February 7, 2025");
+      testOrder2 = new Order("Baguettes", "15 baguettes", 28.99f, "March 1, 2025");
+      testVendor.AddOrder(testOrder1);
+      testVendor.AddOrder(testOrder2);
+    }
+
     [TestMethod]
     public void VendorConstructor_CreatesInstanceOfVendor_Vendor() {
-      // Arrange and Act
       Vendor newVendor = new Vendor();
-
-      // Assert
       Assert.AreEqual(typeof(Vendor), newVendor.GetType());
+    }
+
+    [TestMethod]
+    public void GetName_ReturnsName_String()
+    {
+      string expectedName = "Suzie's Cafe";
+      Assert.AreEqual(testVendor.Name, expectedName);
+    }
+
+    [TestMethod]
+    public void GetDescription_ReturnsDescription_String()
+    {
+      string expectedDescription = "A happy and cozy little cafe!";
+      Assert.AreEqual(testVendor.Description, expectedDescription);
+    }
+
+    [TestMethod]
+    public void GetOrders_ReturnsListOfOrders_OrderList()
+    {
+      Order testOrder1 = new Order("Croissants", "30 boxes of butter croissants", 100.99f, "February 7, 2025");
+      Order testOrder2 = new Order("Baguettes", "15 baguettes", 28.99f, "March 1, 2025");    
+      List<Order> expectedList = new List<Order> { testOrder1, testOrder2 };
+
+      CollectionAssert.AreEqual(testVendor.Orders, expectedList);
     }
 
 
