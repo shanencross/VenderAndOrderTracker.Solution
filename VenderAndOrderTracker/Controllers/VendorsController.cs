2b086a9fa4feaca01d1ebbed01afeda5d6ccd804
Shanen Cross <shanencross@yahoo.com>
2021-05-09 04:27:50 -0700

    Add view for adding new vendor

--- VenderAndOrderTracker/Controllers/VendorsController.cs
+++ VenderAndOrderTracker/Controllers/VendorsController.cs
@@ -12,5 +12,11 @@ namespace VendorAndOrderTracker.Controllers
       List<Vendor> allVendors = Vendor.GetAll();
       return View(allVendors);
     }
+
+    [HttpGet("vendors/new")]
+    public ActionResult New()
+    {
+      return View();     
+    }
   }
 }
